a500e847ada888ec137007850a5d6a24ecd9b179
tutonroy1807 <prosenjitroy1807@gmail.com>
2023-07-17 11:01:10 +0600

    add a multiple element in body and use a container.

--- lib/main.dart
+++ lib/main.dart
@@ -1,87 +1,52 @@
-<<<<<<< HEAD
-=======
-import 'dart:html';
->>>>>>> 7255aff (add a multiple element in body)
+import 'dart:math';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-<<<<<<< HEAD
-void main()
-{
-  runApp(const Myapp());
-}
-
-class Myapp extends StatelessWidget{
-=======
 void main() {
-  runApp(const Myapp());
+  runApp(Myapp());
 }
 
 class Myapp extends StatelessWidget {
->>>>>>> 7255aff (add a multiple element in body)
-  const Myapp({super.key});
+  const Myapp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.lime),
-<<<<<<< HEAD
+      theme: ThemeData(primarySwatch: Colors.orange),
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    );
-  }
-
-}
-
-class HomePage extends StatelessWidget{
-  const HomePage({super.key});
-=======
-        debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: MyPage(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
->>>>>>> 7255aff (add a multiple element in body)
+class MyPage extends StatelessWidget {
+  const MyPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-<<<<<<< HEAD
-      appBar: AppBar(title: Text("Practice App__"),),
-    );
-  }
-
-}
-=======
       appBar: AppBar(
-        title: Text("Pracitce_App--"),
-        centerTitle: true,
-        titleTextStyle: TextStyle(color: Colors.black), // text style add..
+        title: Text("Practice_app--"),
+       titleSpacing: 20,
+        toolbarHeight: 67,
       ),
+
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-
             Container(
-              color: Colors.orange,
-             // margin: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-             // alignment: Alignment.center,
-              //padding: EdgeInsets.only(top: 16, bottom: 15, left: 16, right: 16),
-             // padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-              padding: EdgeInsets.all(10),
-              child: Text("Prosenjit Roy", style: TextStyle(color: Colors.white,
-              fontSize: 20),
-              ),
+              color: Colors.pink,
+              margin: EdgeInsets.all(20),
+            // alignment: Alignment.center,
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              child: Text("Prosenjit Roy", style: TextStyle(color: Colors.black), ),
             )
           ],
         ),
       ),
+
     );
   }
 }
->>>>>>> 7255aff (add a multiple element in body)
